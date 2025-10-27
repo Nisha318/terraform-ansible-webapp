0c7563738ecaf1ebcd6dc16c108d9d727c2eb815
Nisha318 <eunishap@gmail.com>
2025-10-26 20:25:33 -0500

    commit updates

--- infra/main.tf
+++ infra/main.tf
@@ -2,13 +2,13 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.1"
 
-  name               = "${var.project_name}-vpc"
-  cidr               = var.vpc_cidr
+  name = "${var.project_name}-vpc"
+  cidr = var.vpc_cidr
 
-  azs                = var.azs
-  public_subnets     = var.public_subnets
-  private_subnets    = var.private_subnets
-  database_subnets   = var.database_subnets
+  azs              = var.azs
+  public_subnets   = var.public_subnets
+  private_subnets  = var.private_subnets
+  database_subnets = var.database_subnets
 
   enable_nat_gateway                = var.enable_nat
   single_nat_gateway                = var.single_nat_gateway
@@ -17,7 +17,7 @@ module "vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags = { Project = var.project_name }
+  tags             = { Project = var.project_name }
   nat_gateway_tags = { Purpose = "egress" }
 }
 
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,5 +1,5 @@
-output "vpc_id"               { value = module.vpc.vpc_id }
-output "public_subnets"       { value = module.vpc.public_subnets }
-output "private_app_subnets"  { value = module.vpc.private_subnets }
+output "vpc_id" { value = module.vpc.vpc_id }
+output "public_subnets" { value = module.vpc.public_subnets }
+output "private_app_subnets" { value = module.vpc.private_subnets }
 output "private_data_subnets" { value = module.vpc.database_subnets }
-output "bastion_public_ip"    { value = aws_instance.bastion.public_ip }
+output "bastion_public_ip" { value = aws_instance.bastion.public_ip }
