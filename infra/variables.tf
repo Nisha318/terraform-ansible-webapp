11d31f42f7cad0c98874b9722aa51ddec86d816b
Nisha318 <eunishap@gmail.com>
2025-10-27 22:41:54 -0500

    commit updates

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,13 +1,57 @@
-variable "region"            { type = string  default = "us-east-1" }
-variable "project_name"      { type = string }
-variable "vpc_cidr"          { type = string }
-variable "azs"               { type = list(string) }
-variable "public_subnets"    { type = list(string) }
-variable "private_subnets"   { type = list(string) }
-variable "database_subnets"  { type = list(string) }
-variable "enable_nat"        { type = bool }
-variable "single_nat_gateway"{ type = bool }
-variable "key_name"          { type = string }
-variable "admin_cidr"        { type = string }        # use CIDR format, for example "136.244.42.75/32"
-variable "ansible_instance_type" { type = string default = "t3.micro" }
-variable "web_instance_type"     { type = string default = "t3.micro" }
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "database_subnets" {
+  type = list(string)
+}
+
+variable "enable_nat" {
+  type = bool
+}
+
+variable "single_nat_gateway" {
+  type    = bool
+  default = false
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "admin_cidr" {
+  description = "Your admin IP or VPN in CIDR format"
+  type        = string
+  # example: "136.244.42.75/32"
+}
+
+variable "ansible_instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "web_instance_type" {
+  type    = string
+  default = "t3.micro"
+}
